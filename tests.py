2751f0d35825338e2c29d5d21cd87c35d0c22e6d
Brian Serumaga <bsk20111@hotmail.com>
2017-07-07 13:59:48 +0300

    Add test for the learning functionality

--- tests.py
+++ tests.py
@@ -6,7 +6,15 @@ class ProtractTestCases(unittest.TestCase):
     def setUp(self):
         self.example1 = Protrack()
 
-
     def test_for_adding_skill(self):
         self.example1.add_skill('Collaboration')
         self.assertEqual( len(self.example1.all_skills), 1, msg="The skill is not being added")
+
+    def test_for_learning_skill(self):
+        self.example1.add_skill('Communication Skills')
+        self.example1.learn_skill('Communication Skills')
+        self.example1.learn_skill('Collaboration')
+        print(self.example1.all_skills)
+        self.assertTrue(all( obj.progress for obj in self.example1.all_skills), msg="The Learning function is not working")
+
+
