c5d61dfd72f341cb27fc773ce4780709a4407d07
CeciliaCaroline <nalubegac58@gmail.com>
2017-07-07 11:52:48 +0300

    Initial Commit. Pro track app

--- protrack.py
+++ protrack.py
@@ -4,7 +4,6 @@ skills_list = ["collaboration", "integrity"]
 new_skill_list = []
 studied_list = []
 
-
 def add_skills(new_skill):
     # add new skills to our list
     new_skill_list.append(new_skill)
