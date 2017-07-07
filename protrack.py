d518d8bc9e8f9effe2ba4fccc48e76989927b3c9
Brian Serumaga <bsk20111@hotmail.com>
2017-07-07 13:23:39 +0300

    Add the skill add functionality for listing done, all  and undone skills

--- protrack.py
+++ protrack.py
@@ -9,22 +9,28 @@ class Protrack(Skill):
 
     def add_skill(self, name_of_skill):
         skill1 = Skill(name_of_skill)
-        all_skills.append(skill1)
+        self.all_skills.append(skill1)
 
     def learn_skill(self, name_of_skill):
         for obj in self.all_skills:
             if name_of_skill == obj.name:
                 obj.progress = True
 
-
     def list_all_skills(self):
-        pass
+        for obj in self.all_skills:
+            print(obj.name)
+
 
     def list_done_skills(self):
-        pass
+        for obj in self.all_skills:
+            if obj.progress:
+                print(obj.name)
+
 
     def list_undone_skills(self):
-        pass
+        for obj in self.all_skills:
+            if not obj.progress:
+                print(obj.name)
 
     def show_progress(self):
         pass
