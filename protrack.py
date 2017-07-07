2c6ba9861060bbf37ecf33955adb3e6c51fc07f4
Brian Serumaga <bsk20111@hotmail.com>
2017-07-07 13:19:46 +0300

    Add the skill add funtion and the skill learn function

--- protrack.py
+++ protrack.py
@@ -1,5 +1,30 @@
 class Skill(object):
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.progress = False
+
 
 class Protrack(Skill):
-    pass
+    all_skills= []
+
+    def add_skill(self, name_of_skill):
+        skill1 = Skill(name_of_skill)
+        all_skills.append(skill1)
+
+    def learn_skill(self, name_of_skill):
+        for obj in self.all_skills:
+            if name_of_skill == obj.name:
+                obj.progress = True
+
+
+    def list_all_skills(self):
+        pass
+
+    def list_done_skills(self):
+        pass
+
+    def list_undone_skills(self):
+        pass
+
+    def show_progress(self):
+        pass
